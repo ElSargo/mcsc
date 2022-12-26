e624699c3c13a810b9fc48f6851d01e468337b35
El_Sargo <oliversargisonb@gmail.com>
2022-12-26 23:55:22 +1300

    Client refactor

--- src/client.rs
+++ src/client.rs
@@ -5,17 +5,23 @@ pub mod actions {
 
 use actions::{
     controller_client::ControllerClient, AuthAction, AuthRequest, BackupRequest, CommandRequest,
-    DownloadRequest, OpResponce, OpResult, StartRequest, StopRequest,
+    DownloadRequest, StartRequest, StopRequest,
 };
 use common::ran_letters;
 use magic_crypt::{new_magic_crypt, MagicCryptTrait};
+use regex::Regex;
 use serde_derive::Deserialize;
-use std::{fs, io::Write, thread::sleep, time::Duration};
-use tonic::Response;
+use std::{fs, io::Write};
+use tonic::transport::Channel;
+
+use crate::actions::OpResult;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = read_config();
+    let config: Config = {
+        let text = std::fs::read("mcsc_client.toml").expect("No config file!");
+        toml::from_slice(&text).expect("No config file!")
+    };
 
     print!(
         "
@@ -28,161 +34,80 @@ Enter a command: either by name or the number next to it
 4|\"Download\" to download the latest backup
 => "
     );
+    let regex_match = |reg, str| Regex::new(reg).unwrap().is_match(str);
+    let input = read_input();
+    let mut client = ControllerClient::connect(config.ip.to_owned()).await?;
 
-    let mut input = String::new();
-    let _ = std::io::Write::flush(&mut std::io::stdout());
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Could not read input");
+    let response = if regex_match(r"((?i)Start(?-i)|0)", &input) {
+        let token = auth(&mut client, AuthAction::Start, &config).await?;
+        let request = StartRequest { token };
+        client.start(request).await?
+    } else if regex_match(r"((?i)Stop(?-i)|1)", &input) {
+        let token = auth(&mut client, AuthAction::Stop, &config).await?;
+        let request = StopRequest { token };
+        client.stop(request).await?
+    } else if regex_match(r"((?i)Backup(?-i)|2)", &input) {
+        let token = auth(&mut client, AuthAction::Backup, &config).await?;
+        let request = BackupRequest { token };
+        client.backup(request).await?
+    } else if regex_match(r"((?i)Command(?-i)|3)", &input) {
+        print!("Enter command \n=> ");
+        let command = read_input();
+        let token = auth(&mut client, AuthAction::Command, &config).await?;
+        let request = CommandRequest {
+            token,
+            command: command.to_owned(),
+        };
+        client.command(request).await?
+    } else if regex_match(r"((?i)Download(?-i)|4)", &input) {
+        // Generate file name
+        let uuid = ran_letters(32);
+        let path = format!("worldbackup-[{uuid}].tar.gz",);
+        let token = auth(&mut client, AuthAction::Download, &config).await?;
+        let request = DownloadRequest { token };
+        // Download file
+        let mut stream = client.download(request).await?.into_inner();
+        let mut file = fs::File::create(&path)?;
+        while let Some(msg) = stream.message().await? {
+            println!("{}", msg.comment);
 
-    let response: Response<OpResponce>;
-
-    match input.as_str() {
-        // Starts the server, does not wait for it to be ready or for a launch fail
-        "0\n" | "Start\n" | "0\r\n" | "Start\r\n" => {
-            println!("[Awaiting server response...]");
-            let mut client = ControllerClient::connect(config.ip).await?;
-            let key = client
-                .auth(AuthRequest {
-                    action: AuthAction::Start.into(),
-                })
-                .await?
-                .into_inner();
-            println!("[Server connection status: {}]", key.comment);
-            let token = decrypt(key.key, config.key).expect("Client side auth error occurred");
-            let request = StartRequest { token };
-            response = client.start(request).await?;
-        }
-
-        // Stops the server by sending the stop command to stdin
-        "1\n" | "Stop\n" | "1\r\n" | "Stop\r\n" => {
-            println!("[Awaiting server response...]");
-            let mut client = ControllerClient::connect(config.ip).await?;
-            let key = client
-                .auth(AuthRequest {
-                    action: AuthAction::Stop.into(),
-                })
-                .await?
-                .into_inner();
-            println!("[Server connection status: {}]", key.comment);
-            let token = decrypt(key.key, config.key).expect("Client side auth error occurred");
-            let request = StopRequest { token };
-            response = client.stop(request).await?;
-        }
-
-        // Signals the server to backup the world to a compressed archive
-        "2\n" | "Backup\n" | "2\r\n" | "Backup\r\n" => {
-            println!("[Awaiting server response...]");
-            let mut client = ControllerClient::connect(config.ip).await?;
-            let key = client
-                .auth(AuthRequest {
-                    action: AuthAction::Backup.into(),
-                })
-                .await?
-                .into_inner();
-            println!("[Server connection status: {}]", key.comment);
-            let token = decrypt(key.key, config.key)?;
-            let request = BackupRequest { token };
-            response = client.backup(request).await?;
-        }
-
-        // Attempts to run a minecraft command
-        "3\n" | "Command\n" | "3\r\n" | "Command\r\n" => {
-            print!("Enter command \n=> ");
-            let _ = std::io::Write::flush(&mut std::io::stdout());
-            let mut command = String::new();
-            if let Err(_) = std::io::stdin().read_line(&mut command) {
-                println!("Error reading input");
-                return Ok(());
+            // Check for errors
+            match OpResult::from_i32(msg.result) {
+                Some(res) => match res {
+                    OpResult::Success => {}
+                    _ => {
+                        // Throw away redundant file to avoid confusion
+                        let _ = std::fs::remove_file(path);
+                        return Ok(());
+                    }
+                },
+                _ => {}
             }
-            println!("[Awaiting server response...]");
-            let mut client = ControllerClient::connect(config.ip).await?;
-            let key = client
-                .auth(AuthRequest {
-                    action: AuthAction::Command.into(),
-                })
-                .await?
-                .into_inner();
-            println!("[Server connection status: {}]", key.comment);
-            let token = decrypt(key.key, config.key)?;
-            let request = CommandRequest {
-                token,
-                command: command.to_string(),
-            };
-            response = client.command(request).await?;
+
+            file.write(&msg.data)?;
         }
-
-        // Downloads the latest backup from the server
-        "4\n" | "Download\n" | "4\r\n" | "Download\r\n" => {
-            println!("[Awaiting server response...]");
-            // Generate file name
-            let path = format!("worldbackup-[{}].tar.gz", ran_letters(32));
-
-            // Authorize
-            let mut client = ControllerClient::connect(config.ip).await?;
-            let key = client
-                .auth(AuthRequest {
-                    action: AuthAction::Download.into(),
-                })
-                .await?
-                .into_inner();
-            println!("[Server connection status: {}]", key.comment);
-            let token = decrypt(key.key, config.key)?;
-            let request = DownloadRequest { token };
-            // Download file
-            let mut stream = client.download(request).await?.into_inner();
-            let mut file = fs::File::create(&path)?;
-            while let Some(msg) = stream.message().await? {
-                println!("{}", msg.comment);
-
-                // Check for errors
-                match OpResult::from_i32(msg.result) {
-                    Some(res) => match res {
-                        OpResult::Success => {}
-                        _ => {
-                            // Throw away redundant file to avoid confusion
-                            let _ = std::fs::remove_file(path);
-                            return Ok(());
-                        }
-                    },
-                    _ => {}
-                }
-
-                file.write(&msg.data)?;
-            }
-            // Download complete, show location
-            let working_directory = std::env::current_dir();
-            match working_directory {
-                Ok(wdir) => println!("Saved as {:?} {}", wdir, path),
-                Err(_) => println!("Download saved as {}", path),
-            }
-            return Ok(());
+        // Download complete, show location
+        let working_directory = std::env::current_dir();
+        match working_directory {
+            Ok(wdir) => println!("Saved as {:?} {}", wdir, path),
+            Err(_) => println!("Download saved as {}", path),
         }
-
-        // No action recognised
-        _ => {
-            println!("Invalid input");
-            sleep(Duration::from_secs(1));
-            std::process::exit(0);
-        }
+        return Ok(());
     }
+    // No action recognised
+    else {
+        println!("Invalid input");
+        std::process::exit(0);
+    };
     let success = response.into_inner();
-    match success.result {
-        0 => {
-            println!("Success!, server comment: {}", success.comment)
-        }
-        1 => {
-            println!("Failed!, server comment: {}", success.comment)
-        }
-        2 => {
-            println!("Denied!, server comment: {}", success.comment)
-        }
-        _ => {
-            println!("Something fucked up!, server comment: {}", success.comment)
-        }
-    }
+    let status_msg = match success.result {
+        0 => "Success!",
+        1 => "Failed!",
+        2 => "Denied!",
+        _ => "Something fucked up!",
+    };
+    println!("{status_msg}, server comment: {}", success.comment);
 
-    sleep(Duration::from_secs(1));
     Ok(())
 }
 
@@ -192,12 +117,32 @@ struct Config {
     key: String,
 }
 
-fn read_config() -> Config {
-    let text = std::fs::read("mcsc_client.toml").expect("No config file!");
-    toml::from_slice(&text).expect("No config file!")
+fn decrypt(data: &Vec<u8>, key: &str) -> Result<Vec<u8>, magic_crypt::MagicCryptError> {
+    let key = new_magic_crypt!(key, 256);
+    Ok(key.decrypt_bytes_to_bytes(data)?)
 }
 
-pub fn decrypt(data: Vec<u8>, key: String) -> Result<Vec<u8>, magic_crypt::MagicCryptError> {
-    let key = new_magic_crypt!(key, 256);
-    Ok(key.decrypt_bytes_to_bytes(&data)?)
+async fn auth(
+    client: &mut ControllerClient<Channel>,
+    action: AuthAction,
+    config: &Config,
+) -> Result<Vec<u8>, tonic::Status> {
+    println!("[Awaiting server response...]");
+    let key = client
+        .auth(AuthRequest {
+            action: action.into(),
+        })
+        .await?
+        .into_inner();
+    println!("[Server connection status: {}]", key.comment);
+    Ok(decrypt(&key.key, &config.key).expect("Client side auth error occurred"))
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    let _ = std::io::Write::flush(&mut std::io::stdout());
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read input");
+    input
 }
